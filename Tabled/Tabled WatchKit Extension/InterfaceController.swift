425b96fb53b42d3c4c069e33e3c058725413779a
Mark Goody <mgoody@shopkeep.com>
2015-11-23 22:14:50 -0500

    Populate the table

--- Tabled/Tabled WatchKit Extension/InterfaceController.swift
+++ Tabled/Tabled WatchKit Extension/InterfaceController.swift
@@ -4,4 +4,36 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
     @IBOutlet weak var listTable: WKInterfaceTable!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        updateTableitems()
+    }
+    
+    func updateTableitems() {
+        let listOfThings = [
+            "Apple",
+            "Banana",
+            "Pear",
+            "Orange",
+            "Lemon",
+            "Guava",
+            "Melon",
+            "Starfruit",
+            "Grape"
+        ]
+        let numberOfThings = listOfThings.count
+        
+        listTable.setNumberOfRows(numberOfThings, withRowType: "RowController")
+        
+        for i in 0 ..< numberOfThings {
+            let rowController = listTable.rowControllerAtIndex(i) as! RowController
+            rowController.listLabel.setText(listOfThings[i])
+        }
+    }
+    
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        let rowController = listTable.rowControllerAtIndex(rowIndex) as! RowController
+        rowController.listLabel.setTextColor(UIColor.redColor())
+    }
 }
